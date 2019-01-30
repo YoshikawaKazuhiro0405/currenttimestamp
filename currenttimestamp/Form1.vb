d79e0e3d2b5fa3d37752313374f3d72ef8f48471
YoshikawaKazuhiro0405 <qqqa8n6d@oboe.ocn.ne.jp>
2019-01-30 10:09:13 +0900

    プロジェクト ファイルを追加します。

---
+++ currenttimestamp/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim ntim As DateTime = Now()
+        Dim tmst As String = ntim.Year.ToString("0000")
+        tmst &= ntim.Month.ToString("00")
+        tmst &= ntim.Day.ToString("00")
+        tmst &= ntim.Hour.ToString("00")
+        tmst &= ntim.Minute.ToString("00")
+        tmst &= ntim.Second.ToString("00")
+
+        Me.TextBox1.Text = tmst
+
+        'クリップボードに文字列をコピーする
+        Clipboard.SetText(tmst)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim ntim As DateTime = Now()
+        Dim tmst As String = dt2unepocht(ntim)
+        Me.TextBox2.Text = tmst
+
+        'クリップボードに文字列をコピーする
+        Clipboard.SetText(tmst)
+
+    End Sub
+End Class
---
+++ currenttimestamp/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Function dt2unepocht(ByVal vbdate As DateTime) As Long
+        ''VBで使用出来る日付を入力すると、UNIX エポック秒に変換する
+        vbdate = vbdate.ToUniversalTime()
+
+        Dim dt1 As New DateTime(1970, 1, 1, 0, 0, 0, 0)
+        Dim elapsedTime As TimeSpan = vbdate - dt1
+
+        Return CType(elapsedTime.TotalSeconds, Long)
+
+    End Function
+
+End Module
